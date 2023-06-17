857b6273c680448299c2ff2210aac2b20d97e006
dayoung030303 <dayoung3702@naver.com>
2023-06-17 12:07:19 +0900

    message

--- main.sh
+++ main.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
-declare -i num
 
-# 리스트
-IFS_backup="$IFS"
-IFS=$'\n'
-ssh_list=(`cat ssh.txt`)
-key_list=(`cat key.txt`)
-file="./ssh.txt"
-key_file="./key.txt"
-IFS="$IFS_backup"
-
-#문자열 선언
+# 문자열 선언
 str1="= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = ="
- str2="= - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ="
- str3="= -                                                                                             - ="
- str4="= - uu                 *ssssssssss*          *ssssssssss* hh                                    - ="
- str5="= - uu              ssss  uu     ssss    ssss           sshh                                    - ="
- str6="= - uu               ssss  uu     ssss    ssss            hhss                                  - ="
- str7="= - uu                 sssss                sssss         hh   *hhhhhhh*                        - ="
- str8="= -  uu                 uu  ssssssssss         ssssssssss hhh          hh                       - ="
- str9="= -  uu                uu          sssss               ssshhs            hh                     - ="
+str2="= - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ="
+str3="= -                                                                                             - ="
+str4="= - uu                 *ssssssssss*          *ssssssssss* hh                                    - ="
+str5="= - uu              ssss  uu     ssss    ssss           sshh                                    - ="
+str6="= - uu               ssss  uu     ssss    ssss            hhss                                  - ="
+str7="= - uu                 sssss                sssss         hh   *hhhhhhh*                        - ="
+str8="= -  uu                 uu  ssssssssss         ssssssssss hhh          hh                       - ="
+str9="= -  uu                uu          sssss               ssshhs            hh                     - ="
 str10="= -   uu              uu             ssss                shhss            hh                    - ="
 str11="= -    uu          ssss            ssss    ssss           hhss            hh                    - ="
 str12="= -      *uuuuuuuuu*   *ssssssssss*            *sssssssssshhs*            hh                    - ="
@@ -32,54 +22,32 @@ str18="= -                                                                      
 str19="= - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ="
 str20="= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = ="
 
-# 상자 함수
-
+# 상자 출력 함수
 print_box() {
-num=0
-echo "$str1"
-echo "$str2"
-echo "$str3"
-echo "$str4"
-echo "$str5"
-echo "$str6"
-echo "$str7"
-echo "$str8"
-echo "$str9"
-echo "$str10"
-echo "$str11"
-echo "$str12"
-echo "$str13"
-echo "$str14"
-echo "$str15"
-echo "$str16"
+  echo "$str1"
+  echo "$str2"
+  echo "$str3"
+  echo "$str4"
+  echo "$str5"
+  echo "$str6"
+  echo "$str7"
+  echo "$str8"
+  echo "$str9"
+  echo "$str10"
+  echo "$str11"
+  echo "$str12"
+  echo "$str13"
+  echo "$str14"
+  echo "$str15"
+  echo "$str16"
+  echo "$str17"
+  echo "$str18"
+  echo "$str19"
+  echo "$str20"
 }
 
-
-
 # 시작화면 출력
-
-print_box 0
-
-for ((i=1; i<16; i++)); do
-  echo "${list[$i]}"
-done
-
-echo "$str1"
-echo "$str2"
-echo "$str3"
-echo "$str4"
-echo "$str5"
-echo "$str6"
-echo "$str7"
-echo "$str8"
-echo "$str9"
-echo "$str10"
-echo "$str11"
-echo "$str12"
-echo "$str13"
-echo "$str14"
-echo "$str15"
-echo "$str16"
+print_box
 
 # 기능 배열 정의
 str16_1="=                                             [접속하기]                                          ="
@@ -94,24 +62,41 @@ function move_up {
   fi
 }
 
+# 화면 이동 함수
+function move_to_list {
+  clear
+  echo "화면을 Linux.sh로 이동"
+  ./Linux.sh
+  exit 0
+}
+
 # 화살표 방향키 입력 받기
 while true; do
   read -n 1 key
   case "$key" in
-    "") 
-      break;;
-    $'\x1b') 
+    "")
+      if (( current_index2 == 1 )); then
+        move_to_list
+      else
+        current_index2=1
+      fi
+      ;;
+    $'\x1b')
       read -n 1 -t 0.01 -s key
       if [[ "$key" == "[" ]]; then
         read -n 1 -t 0.01 -s key
         case "$key" in
           "A")
             move_up;;
-          "B") 
+          "B")
             move_down;;
         esac
       fi;;
     *)
       ;;
   esac
+  # 상자 출력
+  clear
+  print_box
+  echo "${array2[current_index2]}"
 done
